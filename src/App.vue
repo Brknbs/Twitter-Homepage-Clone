23a6f7632aab5c74b0c39f4eb01d2f745f86ab32
Berkan <brknbs@gmail.com>
2020-09-14 02:44:12 +0300

    trendings done

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="app" class="flex container h-screen w-full">
+    <!-- navbar -->
     <div class="lg:w-1/5 border-r border-lighter px-2 lg:px-6 py-2 flex flex-col justify-between">
       <div>
         <button class="h-12 w-12 hover:bg-lightblue text-3xl rounded-full text-blue mb-3 focus:outline-none">
@@ -11,7 +12,7 @@
             <p class="text-lg font-bold text-left hidden lg:block">{{ tab.title }}</p>
           </button>
         </div>
-        <button class="text-white bg-blue rounded-full font-semibold w-12 h-12 lg:h-auto lg:w-full p-3 focus:outline-none hover:bg-darkblue">
+        <button class="text-white bg-blue rounded-full font-semibold w-12 h-12 lg:h-auto lg:w-full p-3 focus:outline-none hover:bg-darkblue mb-3">
           <p class="hidden lg:block">Tweet</p>
           <i class="fas fa-plus lg:hidden"></i>
         </button>
@@ -43,6 +44,52 @@
         </div>
       </div>
     </div>
+
+    <!-- tweets -->
+    <div class="w-1/2 h-full">
+
+    </div>
+
+    <!-- trendings -->
+    <div class="md:block hidden w-1/3 h-full border-l border-lighter py-2 px-6 overflow-y-scroll relative">
+      <input type="text" class="rounded-full w-full p-2 bg-lighter focus:outline-none pl-10 text-sm focus:bg-white border focus:border-blue" placeholder="Search Twitter">
+      <i class="fas fa-search absolute left-0 top-0 mt-5 ml-10 text-sm text-light"></i>
+      <div class="w-full rounded-lg bg-lightest mt-6">
+        <div class="flex items-center justify-between px-3 py-1">
+          <p class="text-xl font-extrabold">Trends for you</p>
+          <div class="hover:bg-lighter rounded-full">
+            <i class="fas fa-cog text-lg text-blue cursor-pointer p-3"></i>
+          </div>
+        </div>
+        <button v-for="trend in trending" :key="trend.title" class="w-full flex justify-between hover:bg-lighter p-3 border-t border-lighter focus:outline-none">
+          <div>
+            <p class="text-sm text-left leading-tight text-dark pb-1">{{ trend.top }}</p>
+            <p class="font-bold text-left leading-tight pb-2">#{{ trend.title }}</p>
+            <p class="text-sm text-left leading-tight text-dark">{{ trend.bottom }}</p>
+          </div>
+          <i class="fas fa-angle-down text-lg font-semibold text-dark"></i>
+        </button>
+        <button class="p-3 w-full hover:bg-lighter text-left text-blue border-t border-lighter focus:outline-none">
+          Show more
+        </button>
+      </div>
+      <div class="w-full rounded-lg bg-lightest mt-6">
+        <div class="px-3 py-2">
+          <p class="text-xl font-extrabold">Who to follow</p>
+        </div>
+        <button v-for="friend in friends" :key="friend.handle" class="w-full flex items-center hover:bg-lighter p-3 border-t border-lighter focus:outline-none">
+          <img :src="require(`${friend.src}`)" :name="friend.name" :alt="friend.name" class="w-12 h-12 rounded-full"> 
+          <div class="ml-4">
+            <p class="text-sm font-bold leading-tight text-left pb-1"> {{ friend.name }} </p>
+            <p class="text-sm leading-tight text-dark text-left"> {{ friend.handle }} </p>
+          </div>
+          <button class="ml-auto text-sm text-blue py-1 px-4 rounded-full border-2 border-blue">Follow</button>
+        </button>
+        <button class="p-3 w-full hover:bg-lighter text-left text-blue border-t border-lighter">
+          Show more
+        </button>
+      </div>
+    </div>
     
   </div>
 </template>
@@ -62,7 +109,19 @@ export default {
         {icon: 'fas fa-ellipsis-h', title: 'More', id: 'more'}
       ],
       id: '',
-      dropdown: false
+      dropdown: false,
+      trending: [
+        {top: 'Trending in TX', title: 'Gigi', bottom: 'Trending with: Rip Gigi'},
+        {top: 'Music', title: 'WeWon', bottom: '135K Tweets'},
+        {top: 'Pop', title: 'BlueIvy', bottom: '40k tweets'},
+        {top: 'Trending in US', title: 'DenimDay', bottom: '40k tweets'},
+        {top: 'Trending', title: 'WhenBeyonce', bottom: '25.4k tweets'},
+      ],
+      friends: [
+        {src: "./assets/images/elonmusk.jpg", name: 'Elon Musk', handle: '@teslaBoy'},
+        {src: './assets/images/ronaldo.jpg', name: 'Cristiano Ronaldo', handle: '@Cristiano'},
+        {src: './assets/images/marshmello.jpg', name: 'Marshmello', handle: '@marshmellomusic'}
+      ],
     }
   }
 }
