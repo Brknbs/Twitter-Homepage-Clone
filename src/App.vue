098bbfc50c191e293a2bdde823626cb27b7f9367
Berkan <brknbs@gmail.com>
2020-09-13 22:23:04 +0300

    tailwind added some of navbar created

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,39 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+  <div id="app" class="flex container h-screen w-full">
+    <div class="w-1/5 border-r border-lighter px-8 py-2">
+      <button class="h-12 w-12 hover:bg-lightblue text-3xl rounded-full text-blue mb-3">
+        <i class="fab fa-twitter"></i>
+      </button>
+      <div>
+        <button @click="id = tab.id" v-for="tab in tabs" :key="tab.id" :class="`flex items-center py-2 px-4 hover:bg-lightblue rounded-full mr-auto hover:text-blue mb-3 focus:outline-none ${id === tab.id ? 'text-blue' : ''}`">
+          <i :class="`${tab.icon} text-2xl mr-4 text-left`"></i>
+          <p class="text-lg font-bold text-left">{{ tab.title }}</p>
+        </button>
+      </div>
     </div>
-    <router-view/>
   </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
+<script>
+export default {
+  data() {
+    return {
+      tabs: [
+        {icon: 'fas fa-home', title: 'Home', id:'home'},
+        {icon: 'fas fa-hashtag', title: 'Explore', id: 'explore'},
+        {icon: 'far fa-bell', title: 'Notifications', id: 'notifications'},
+        {icon: 'far fa-envelope', title: 'Messages', id: 'messages'},
+        {icon: 'far fa-bookmark', title: 'Bookmarks', id: 'bookmarks'},
+        {icon: 'fas fa-clipboard-list', title: 'Lists', id: 'lists'},
+        {icon: 'far fa-user', title: 'Profile', id: 'profile'},
+        {icon: 'fas fa-ellipsis-h', title: 'More', id: 'more'}
+      ],
+      id: ''
     }
   }
 }
+</script>
+
+<style lang="scss">
+
 </style>
